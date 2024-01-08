0b28c19663e3bbc4eb4215119b62e1d7e25c22be
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2024-01-08 15:28:08 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+// const sequelize = require('./config/db')
+// const models = require('./models')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// const router = require('./routes/index')
+const path = require('path')
+const PORT = process.env.PORT || 5001
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'images')))
+// app.use('/api', router)
+
+const start = async () => {
+    try {
+        // await sequelize.authenticate()
+        // await sequelize.sync({ alter: true })
+        app.listen(PORT, () => console.log(`App started on port ${PORT}`))
+    } catch (e) {
+        console.error(e.message)
+    }
+}
+start()
---
+++ config/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialectModule: require('mysql2'),
+    max: 30, // максимальное количество соединений в пуле
+    idleTimeoutMillis: 60000, // время ожидания до закрытия неиспользуемого соединения
+    connectionTimeoutMillis: 2000,
+    dialectOptions: {
+        connectTimeout: 5000, // set connect timeout to 5 seconds
+        requestTimeout: 5000, // set request timeout to 5 seconds
+        timezone: '+06:00',
+    },
+})
