fd505e6489fc021d78bfe5ff42d5be4dff00d6bb
nyanpasa <36734086+XZID@users.noreply.github.com>
2024-01-25 15:25:37 +0600

    Update errorHandler.js

--- filters/errorHandler.js
+++ filters/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-
+    console.log(err)
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
