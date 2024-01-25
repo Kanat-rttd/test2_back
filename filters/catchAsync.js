6a032421a2e28ef2f8f7b89dbf81cd6ea79f4496
nyanpasa <36734086+XZID@users.noreply.github.com>
2024-01-25 15:39:45 +0600

    Update catchAsync.js

--- filters/catchAsync.js
+++ filters/catchAsync.js
@@ -1,17 +1,20 @@
-/**
- * Create an error-handling middleware wrapper.
- * @param {Function} fn - The asynchronous function to be wrapped.
- * @returns {Function} - The middleware function.
- */
-module.exports = function (fn) {
-    /**
-     * Middleware function for error handling.
-     * @param {Object} req - The Express request object.
-     * @param {Object} res - The Express response object.
-     * @param {Function} next - The next middleware function.
-     */
-    return (req, res, next) => {
-        // Execute the provided async function and handle any errors
-        fn(req, res, next).catch(next)
+const { ValidationError } = require('sequelize')
+const ApiError = require('../helpers/ApiError')
+
+function catchAsync(controllerFunction) {
+    return async (req, res, next) => {
+        try {
+            await controllerFunction(req, res, next)
+        } catch (err) {
+            if (err instanceof ValidationError) {
+                const errors = Object.values(err.errors).map((e) => e.message)
+                res.status(400).json({ message: errors[0] })
+            } else {
+                next(ApiError.badRequest(err.message))
+                res.status(400).send('Application error contact support')
+            }
+        }
     }
 }
+
+module.exports = catchAsync
