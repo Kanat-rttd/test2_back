c5ea30f6c60532b4ea0ef899a714b96d3fca3822
Arystanmxn <100197181+Arystanmxn@users.noreply.github.com>
2024-05-10 11:43:20 +0500

    add new error handler

--- filters/errorHandler.js
+++ filters/errorHandler.js
@@ -1,11 +1,23 @@
-const errorHandler = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
+import { ValidationError, UniqueConstraintError } from 'sequelize'
+import ApiError from './ApiError.js'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    })
+function errorHandler(fn) {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((err) => {
+            console.log(err)
+            console.log(err.message)
+            if (err instanceof UniqueConstraintError) {
+                let customMessage = 'Ошибка уникальности данных'
+                console.log(err)
+                res.status(400).json({ message: customMessage })
+            } else if (err instanceof ValidationError) {
+                const errors = Object.values(err.errors).map((e) => e.message)
+                res.status(400).json({ message: errors[0] })
+            } else {
+                next(ApiError.badRequest(err.message))
+                res.status(400).send({ message: err.message })
+            }
+        })
+    }
 }
-
 module.exports = errorHandler
