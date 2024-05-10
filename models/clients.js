545e2159358ee15f52f53b9683bce008540e5740
Karina <karina@mxn.kz>
2024-05-10 12:47:09 +0500

    add infex for name/contact for clients

--- models/clients.js
+++ models/clients.js
@@ -1,15 +1,34 @@
 const sequelize = require('../config/db')
 const { DataTypes } = require('sequelize')
 
-const clients = sequelize.define('clients', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING },
-    surname: { type: DataTypes.STRING },
-    contact: { type: DataTypes.STRING },
-    telegrammId: { type: DataTypes.STRING },
-    password: { type: DataTypes.STRING },
-    status: { type: DataTypes.STRING },
-    isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
-})
+const clients = sequelize.define(
+    'clients',
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        name: { type: DataTypes.STRING },
+        surname: { type: DataTypes.STRING },
+        contact: { type: DataTypes.STRING },
+        telegrammId: { type: DataTypes.STRING },
+        password: { type: DataTypes.STRING },
+        status: { type: DataTypes.STRING },
+        isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+    },
+    {
+        indexes: [
+            {
+                unique: true,
+                fields: ['contact'],
+                name: 'phone_unique_constraint',
+                msg: 'Пользователь с таким телефоном уже существует',
+            },
+            {
+                unique: true,
+                fields: ['name'],
+                name: 'name_unique_constraint',
+                msg: 'Пользователь с таким именем уже существует',
+            },
+        ],
+    },
+)
 
 module.exports = clients
