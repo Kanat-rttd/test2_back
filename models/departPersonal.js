28707411e2b0b02aac769e895a3b08c68fe1644f
Karina <karina@mxn.kz>
2024-05-13 11:32:22 +0500

    add infex for name for depart personal

--- models/departPersonal.js
+++ models/departPersonal.js
@@ -1,13 +1,26 @@
 const sequelize = require('../config/db')
 const { DataTypes } = require('sequelize')
 
-const departPersonal = sequelize.define('departPersonal', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING },
-    surname: { type: DataTypes.STRING },
-    status: { type: DataTypes.STRING },
-    userClass: { type: DataTypes.STRING },
-    fixSalary: { type: DataTypes.STRING },
-})
+const departPersonal = sequelize.define(
+    'departPersonal',
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        name: { type: DataTypes.STRING },
+        surname: { type: DataTypes.STRING },
+        status: { type: DataTypes.STRING },
+        userClass: { type: DataTypes.STRING },
+        fixSalary: { type: DataTypes.STRING },
+    },
+    {
+        indexes: [
+            {
+                unique: true,
+                fields: ['name'],
+                name: 'name_unique_constraint',
+                msg: 'Пользователь с таким именем уже существует',
+            },
+        ],
+    },
+)
 
 module.exports = departPersonal
