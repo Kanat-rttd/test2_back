e5aa2fac42cdbbe4abacc001d6012b0f09e74cf9
Karina <karina@mxn.kz>
2024-05-22 11:28:13 +0500

    added "put" route to baking.route.js

--- routes/baking.route.js
+++ routes/baking.route.js
@@ -6,6 +6,7 @@ let router = Router()
 
 router.get('/', catchAsync(Controller.getAll))
 router.post('/', catchAsync(Controller.createBaking))
+router.put('/:id', catchAsync(Controller.updateBaking))
 router.delete('/:id', catchAsync(Controller.deleteBaking))
 
 module.exports = router
