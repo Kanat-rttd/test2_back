83c80dd4a1e168c7717cfc88e7751f2af26d058e
Karina <karina@mxn.kz>
2024-06-07 11:50:57 +0500

    added filter by type

--- controllers/financeCategories.controller.js
+++ controllers/financeCategories.controller.js
@@ -2,8 +2,16 @@ const models = require('../models')
 
 class FinanceCategories {
     async getAll(req, res, next) {
+        const {type} = req.query
+
+        let filterOptions = {}
+
+        if(type){
+            filterOptions.type = type
+        }
         const data = await models.financeCategories.findAll({
             attributes: ['id', 'name', 'type'],
+            where: filterOptions,
         })
         return res.json(data)
     }
