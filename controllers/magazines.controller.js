5b53034edd397d82f0a512e34e53086758cb7236
Karina <karina@mxn.kz>
2024-06-19 16:13:14 +0500

    changed magazines.controller

--- controllers/magazines.controller.js
+++ controllers/magazines.controller.js
@@ -36,34 +36,42 @@ class MagazinesController {
     async createMagazine(req, res, next) {
         const magazineData = req.body
 
-        const existingMagazine = await models.magazines.findOne({ where: { isDeleted: false, name: magazineData.data.name }});
-        if(existingMagazine != null){
-            console.log(existingMagazine);
-            return res.status(409).json({message: 'Магазин уже существует', data: existingMagazine});
+        const existingMagazine = await models.magazines.findOne({
+            where: { isDeleted: false, name: magazineData.data.name },
+        })
+        if (existingMagazine != null) {
+            console.log(existingMagazine)
+            return res.status(409).json({ message: 'Такой магазин уже существует', data: existingMagazine })
         }
 
-        const tr = await sequelize.transaction();
+        const tr = await sequelize.transaction()
 
-        try{
-            const createdMagazine = await models.magazines.create({
-                name: magazineData.data.name,
-                clientId: magazineData.data.clientId,
-                status: magazineData.data.status,
-            },{ transaction: tr });
-    
-            const createdContragent = await models.contragent.create({
-                contragentName: magazineData.data.name,
-                status: magazineData.data.status,
-                mainId: createdMagazine.id,
-                type: 'магазин',
-            },{ transaction: tr })
+        try {
+            const createdMagazine = await models.magazines.create(
+                {
+                    name: magazineData.data.name,
+                    clientId: magazineData.data.clientId,
+                    status: magazineData.data.status,
+                },
+                { transaction: tr },
+            )
 
-            await tr.commit();
-    
+            const createdContragent = await models.contragent.create(
+                {
+                    contragentName: magazineData.data.name,
+                    status: magazineData.data.status,
+                    mainId: createdMagazine.id,
+                    type: 'магазин',
+                },
+                { transaction: tr },
+            )
+
+            await tr.commit()
+
             return res.status(200).json({ message: 'Магазин успешно создан', data: createdMagazine })
-        }catch(ex){
-            console.log(ex);
-            await tr.rollback();
+        } catch (error) {
+            console.log(error)
+            await tr.rollback()
         }
     }
 
@@ -78,48 +86,74 @@ class MagazinesController {
             status: magazineData.status,
         }
 
-        const findedMagazine = await models.magazines.findByPk(id)
-
-        await models.contragent.update(
-            { contragentName: magazineData.name, status: magazineData.status },
-            { where: { contragentName: findedMagazine.name } },
-        )
-
-        const updatedMagazine = await models.magazines.update(updateObj, {
-            where: {
-                id,
-            },
-            individualHooks: true,
+        const existingMagazine = await models.magazines.findOne({
+            where: { isDeleted: false, name: magazineData.name },
         })
+        if (existingMagazine != null) {
+            console.log(existingMagazine)
+            return res.status(409).json({ message: 'Такой магазин уже существует', data: existingMagazine })
+        }
 
-        return res.status(200).json({ message: 'Магазин успешно обновлен', data: updatedMagazine })
+        const tr = await sequelize.transaction()
+
+        try {
+            const findedMagazine = await models.magazines.findByPk(id)
+
+            await models.contragent.update(
+                { contragentName: magazineData.name, status: magazineData.status },
+                { where: { contragentName: findedMagazine.name } },
+            )
+
+            const updatedMagazine = await models.magazines.update(updateObj, {
+                where: {
+                    id,
+                },
+                individualHooks: true,
+            })
+            await tr.commit()
+
+            return res.status(200).json({ message: 'Магазин успешно обновлен', data: updatedMagazine })
+        } catch (error) {
+            console.log(error)
+            await tr.rollback()
+        }
     }
 
     async deleteMagazine(req, res, next) {
         const { id } = req.params
 
-        const findedMagazine = await models.magazines.findByPk(id)
+        const tr = await sequelize.transaction()
 
-        await models.contragent.update(
-            {
-                isDeleted: true,
-            },
-            {
-                where: { contragentName: findedMagazine.name },
-            },
-        )
+        try {
+            const findedMagazine = await models.magazines.findByPk(id)
 
-        const deletedMagazine = await models.magazines.update(
-            {
-                isDeleted: true,
-            },
-            {
-                where: {
-                    id,
+            await models.contragent.update(
+                {
+                    isDeleted: true,
                 },
-            },
-        )
-        return res.status(200).json({ message: 'Магазин успешно удален', data: deletedMagazine })
+                {
+                    where: { contragentName: findedMagazine.name },
+                },
+            )
+
+            const deletedMagazine = await models.magazines.update(
+                {
+                    isDeleted: true,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            )
+
+            await tr.commit()
+
+            return res.status(200).json({ message: 'Магазин успешно удален', data: deletedMagazine })
+        } catch (error) {
+            console.log(error)
+            await tr.rollback()
+        }
     }
 }
 
