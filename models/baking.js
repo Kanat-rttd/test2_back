64268a84014372fe2b14af722ad810f66a40095c
bakhtiyarMxn <bakhtiyar@mxn.kz>
2024-06-25 16:32:24 +0500

    Update baking.js

--- models/baking.js
+++ models/baking.js
@@ -4,11 +4,7 @@ const product = require('./products')
 
 const baking = sequelize.define('baking', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    flour: { type: DataTypes.FLOAT },
-    salt: { type: DataTypes.FLOAT },
-    yeast: { type: DataTypes.FLOAT },
-    malt: { type: DataTypes.FLOAT },
-    butter: { type: DataTypes.FLOAT },
+    productId: { type: DataTypes.INTEGER },
     temperature: { type: DataTypes.FLOAT },
     dateTime: { type: DataTypes.DATE },
     output: { type: DataTypes.FLOAT },
@@ -16,8 +12,31 @@ const baking = sequelize.define('baking', {
     isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
 })
 
-product.hasMany(baking)
+const bakingDetails = sequelize.define('bakingDetails', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    bakingId: { type: DataTypes.INTEGER },
+    goodsCategoryId: { type: DataTypes.INTEGER },
+    quantity: { type: DataTypes.FLOAT },
+})
 
+product.hasMany(baking)
 baking.belongsTo(product)
 
-module.exports = baking
+baking.hasMany(bakingDetails)
+bakingDetails.belongsTo(baking)
+
+module.exports = { baking, bakingDetails }
+
+// const baking = sequelize.define('baking', {
+//     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//     flour: { type: DataTypes.FLOAT },
+//     salt: { type: DataTypes.FLOAT },
+//     yeast: { type: DataTypes.FLOAT },
+//     malt: { type: DataTypes.FLOAT },
+//     butter: { type: DataTypes.FLOAT },
+//     temperature: { type: DataTypes.FLOAT },
+//     dateTime: { type: DataTypes.DATE },
+//     output: { type: DataTypes.FLOAT },
+//     defective: { type: DataTypes.FLOAT },
+//     isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+// })
