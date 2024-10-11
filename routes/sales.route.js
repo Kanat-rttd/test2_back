7f4b6874d9e98bdff44dcff148e8d479cd69684a
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-10-11 14:12:11 +0500

    #67

--- routes/sales.route.js
+++ routes/sales.route.js
@@ -10,10 +10,10 @@ router.get('/', catchAsync(Controller.getAll))
 router.post('/', catchAsync(Controller.createSale))
 router.put('/:id', catchAsync(Controller.updateSale))
 router.delete('/:id', catchAsync(Controller.deleteSale))
+router.get('/client', getUser, catchAsync(Controller.getByClient))
 router.get('/:id', catchAsync(Controller.getOrderById))
 router.put('/status/:id', catchAsync(Controller.setDoneStatus))
 router.post('/status/test', catchAsync(Controller.getByFacilityUnit))
 router.put('/order/:id', catchAsync(Controller.saveOrderChanges))
-router.get('/client', getUser, catchAsync(Controller.getByClient))
 
 module.exports = router
