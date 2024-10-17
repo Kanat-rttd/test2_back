3a26b1071e6293140130828a92b4e94a38d30724
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-10-17 12:54:52 +0500

    #41 update view

--- views/magazineDebt.sql
+++ views/magazineDebt.sql
@@ -1,15 +1,22 @@
 SELECT
-    c.contragentname                     AS MagazineName,
-    COALESCE(SUM(DISTINCT dt.amount), 0) - COALESCE(SUM(DISTINCT f_payments.amount), 0) -
-    COALESCE(SUM(DISTINCT kt.amount), 0) AS Debit
+    c.contragentName                                                                AS MagazineName,
+    COALESCE(dt.sum, 0) - COALESCE(SUM(f_payments.amount), 0) - COALESCE(kt.sum, 0) AS Debit
 FROM
     contragents c
-    LEFT JOIN debtTransfers kt ON c.id = kt.kt
-    LEFT JOIN debtTransfers dt ON c.id = dt.dt
+    LEFT JOIN (
+        SELECT kt AS kt, SUM(amount) AS sum
+        FROM debtTransfers
+        GROUP BY kt
+    ) kt ON kt.kt = c.id
+    LEFT OUTER JOIN (
+        SELECT dt AS dt, SUM(amount) AS sum
+        FROM debtTransfers
+        GROUP BY dt
+    ) dt ON dt.dt = c.id
     LEFT JOIN finances f_payments ON c.id = f_payments.contragentid AND f_payments.financecategoryid IN (
-        SELECT financeCategories.id FROM financeCategories WHERE (financeCategories.name = 'Оплата от магазинов')
+        SELECT financeCategories.id FROM financeCategories WHERE financeCategories.name = 'Оплата от магазинов'
     )
 WHERE
     c.contragenttypeid = '4'
 GROUP BY
-    c.contragentname
+    c.contragentname, dt.sum, kt.sum;
