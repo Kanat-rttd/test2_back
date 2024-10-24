0a14c1eff095f442fff2751cbcfa24a203417322
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-10-24 11:07:08 +0500

    #44

--- models/goodsDispatchDetails.js
+++ models/goodsDispatchDetails.js
@@ -1,14 +1,13 @@
 const sequelize = require('../config/db')
 const { DataTypes } = require('sequelize')
 const products = require('./products')
-const goodsDispatch = require('./goodsDispatch')
 
 const goodsDispatchDetails = sequelize.define('goodsDispatchDetails', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     goodsDispatchId: { type: DataTypes.INTEGER },
     productId: { type: DataTypes.INTEGER },
-    quantity: { type: DataTypes.INTEGER },
-    price: { type: DataTypes.STRING },
+    quantity: { type: DataTypes.INTEGER, allowNull: false },
+    price: { type: DataTypes.STRING, allowNull: false },
 })
 
 products.hasMany(goodsDispatchDetails)
