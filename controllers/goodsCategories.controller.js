5ffd73ba08c5b7feaabd42ff3fcbbc817b4e5339
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-12-24 10:39:09 +0500

    feat: :sparkles: add endpoints to CUD goods categories

--- controllers/goodsCategories.controller.js
+++ controllers/goodsCategories.controller.js
@@ -1,6 +1,11 @@
 const models = require('../models')
 
 class GoodsCategories {
+    /**
+     *
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
     async getAll(req, res, next) {
         const { categoryId } = req.query
 
@@ -10,10 +15,64 @@ class GoodsCategories {
             filterOptions.id = categoryId
         }
         const data = await models.goodsCategories.findAll({
-            attributes: ['id', 'category', 'unitOfMeasure'],
             where: { ...filterOptions },
         })
         return res.json(data)
+    }
+
+    /**
+     *
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async create(req, res) {
+        const { category, unitOfMeasure } = req.body
+
+        const goodsCategory = await models.goodsCategories.create({
+            category,
+            unitOfMeasure,
+        })
+
+        return res.status(200).json({
+            message: 'Категория успешно создана',
+            goodsCategory,
+        })
+    }
+
+    /**
+     *
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async update(req, res) {
+        const { id } = req.params
+        const { category, unitOfMeasure } = req.body
+
+        await models.goodsCategories.update(
+            { category, unitOfMeasure },
+            {
+                where: { id },
+            },
+        )
+
+        return res.status(200).json({
+            message: 'Категория успешно обновлена',
+        })
+    }
+
+    /**
+     *
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async delete(req, res) {
+        const { id } = req.params
+
+        await models.goodsCategories.destroy({ where: { id } })
+
+        return res.status(200).json({
+            message: 'Категория успешно удалена',
+        })
     }
 }
 
--- routes/goodsCategories.route.js
+++ routes/goodsCategories.route.js
@@ -3,8 +3,11 @@ const { Router } = require('express')
 const Controller = require('../controllers/goodsCategories.controller')
 const catchAsync = require('../filters/catchAsync')
 
-let router = Router()
+const router = Router()
 
 router.get('/', catchAsync(Controller.getAll))
+router.post('/', catchAsync(Controller.create))
+router.put('/:id', catchAsync(Controller.update))
+router.delete('/:id', catchAsync(Controller.delete))
 
 module.exports = router
