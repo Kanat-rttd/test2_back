573f0a3de447499d8a8bda20a628b48369ea430f
Edelmir Muratkanov <muratkanov02@gmail.com>
2024-12-30 12:50:04 +0500

    feat: :sparkles: update debt calculation view
    because report view was changed
    
    #88

--- views/debtCalculation.sql
+++ views/debtCalculation.sql
@@ -1,13 +1,12 @@
 SELECT
-    ClientName    AS client,
-    SUM(Sales)    AS sales,
-    SUM(Returns)  AS returns,
+    ClientName AS client,
+    SUM(Sales) AS sales,
+    SUM(Returns) AS returns,
     SUM(Overhead) AS overhead,
-    SUM(Expenses) AS expenses,
     SUM(Payments) AS payments,
-    SUM(Credit)   AS credit,
-    SUM(Debit)    AS debit,
-    SUM(Debt)     AS debt
+    SUM(Credit) AS credit,
+    SUM(Debit) AS debit,
+    SUM(Debt) AS debt
 FROM
     ReportView
 GROUP BY
